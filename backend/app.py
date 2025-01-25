89f659fdafa73bdf61540a56c1cac8e3920e7e32
Julia Chen <jchen319@terpmail.umd.edu>
2025-01-25 12:58:41 -0500

    using gemini

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,50 @@
 import os
-
 from flask import Flask, request, render_template
 from flask_cors import CORS
 from dotenv import load_dotenv
 from pymongo import MongoClient
-
 from model import RecipeModel
 
+import google.generativeai as genai
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:3000"])
 
 load_dotenv("../.env", override=True)
+
 MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
-
 db = client["coke"]
 recipes = db["recipes"]
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+model = genai.GenerativeModel("gemini-1.5-flash")
 
 
 @app.route("/")
 def home():
     return render_template("recipe.html")
+
+
+def process_output(output):
+    recipe_dict = {"name": "test", "ingredients": [], "instructions": []}
+    return recipe_dict
+
+
+@app.route("/generate_recipe", methods=["POST", "GET"])
+def generate_recipe():
+    # Retrieve the user input from the previous page
+    theme = request.args.get(
+        "theme", "default theme"
+    )  # Replace 'default theme' with a fallback value if necessary
+
+    # Prompt to be Gemini
+    prompt = f"Generate a drink recipe that only uses brands owned by Coca-Cola with no extra non-Coca-Cola-owned ingredients. I want the name, ingredients, and numbered instructions in that order with no extra text. Adjust the amounts for something that is suitable to serve one person. Make it fit this theme: {theme}"
+
+    response = model.generate_content(prompt)
+    print(response.text)
+
+    return RecipeModel(**process_output(response.text)).to_json()
 
 
 @app.post("/recipes")
