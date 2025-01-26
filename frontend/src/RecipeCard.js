8df9c45d476a82838c9972db54552b0d9debb5c3
leeannn-s <leeannshaw05@gmail.com>
2025-01-25 22:23:51 -0500

    recipe font look nice

--- frontend/src/RecipeCard.js
+++ frontend/src/RecipeCard.js
@@ -4,8 +4,7 @@ import FavoriteToggle from './FavoriteToggle';
 
 function RecipeCard({ recipe, includeToggle }) {
     return (
-        <Card sx={{ margin:"10px", backgroundColor: '#ffe3ea' }}>
-        <Card>
+        <Card sx={{ margin: "10px", backgroundColor: '#fae6e7' }}>
             <CardContent>
                 <Box
                     sx={{
@@ -14,15 +13,26 @@ function RecipeCard({ recipe, includeToggle }) {
                         width: '100%',
                     }}
                 >
-                    <Typography variant="h5">{recipe.name}</Typography></Box>
-                Ingredients:
+                    <Typography variant="h5" sx={{ fontFamily: 'Fira Sans', fontWeight: 'bold' }}>
+                        {recipe.name}
+                    </Typography>
+                </Box>
+                <Typography variant="h6" sx={{ fontFamily: 'Fira Sans', marginTop: 2, fontWeight:'bold' }}>
+                    Ingredients:
+                </Typography>
                 {recipe.ingredients.map((ingredient, index) => (
-                    <Typography variant="body2" key={index}>- {ingredient.amount} {ingredient.unit} {ingredient.name}</Typography>
+                    <Typography variant="body2" key={index} sx={{ fontFamily: 'Fira Sans' }}>
+                        - {ingredient.amount} {ingredient.unit} {ingredient.name}
+                    </Typography>
                 ))}
 
-                Instructions:
+                <Typography variant="h6" sx={{ fontFamily: 'Fira Sans', marginTop: 2, fontWeight:'bold'}}>
+                    Instructions:
+                </Typography>
                 {recipe.instructions.map((instruction, index) => (
-                    <Typography variant="body2" key={index}>{instruction}</Typography>
+                    <Typography variant="body2" key={index} sx={{ fontFamily: 'Fira Sans' }}>
+                        {instruction}
+                    </Typography>
                 ))}
                 {includeToggle && <FavoriteToggle itemId={recipe._id} initialFavorite={recipe.favorited} />}
             </CardContent>
